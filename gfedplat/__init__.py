48c5c7414e5dc2d9df595368a139d7d46ff7ae62
Anonymous-UNITE <noorainkorai01@gmail.com>
2025-11-22 21:42:27 +1100

    Add files via upload

---
+++ gfedplat/__init__.py
@@ -0,0 +1,37 @@
+
+from gfedplat.main import initialize, read_params, outFunc
+import os
+
+from gfedplat.Algorithm import Algorithm
+from gfedplat.Client import Client
+from gfedplat.DataLoader import DataLoader
+from gfedplat.Module import Module
+from gfedplat.Metric import Metric
+from gfedplat.seed import setup_seed
+
+from gfedplat.metric.Correct import Correct
+from gfedplat.metric.Precision import Precision
+from gfedplat.metric.Recall import Recall
+
+from gfedplat.model.CNN import CNN
+from gfedplat.model.MLP import MLP
+from gfedplat.model.ResNet import ResNet18, ResNet50
+
+
+import gfedplat.algorithm
+from gfedplat.algorithm.UNITE.UNITE import UNITE
+
+from gfedplat.dataloaders.separate_data import separate_data, create_data_pool
+from gfedplat.dataloaders.DataLoader_cifar10_dir import DataLoader_cifar10_dir
+from gfedplat.dataloaders.DataLoader_fashion_dir import DataLoader_fashion_dir
+from gfedplat.dataloaders.DataLoader_cifar100_dir import DataLoader_cifar100_dir
+
+
+data_folder_path = os.path.dirname(os.path.abspath(__file__)) + '/data/'
+if not os.path.exists(data_folder_path):
+    os.makedirs(data_folder_path)
+
+
+pool_folder_path = os.path.dirname(os.path.abspath(__file__)) + '/pool/'
+if not os.path.exists(pool_folder_path):
+    os.makedirs(pool_folder_path)
